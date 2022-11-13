b6afd8e90928015bb4eb57ee5a3cb55e1b119392
heecheol.park <heecheol.park@linecorp.com>
2022-11-13 19:56:22 +0900

    Update main.py

--- python_make_words/main.py
+++ python_make_words/main.py
@@ -6,17 +6,16 @@ from words import (
 
 
 def main():
-    chapters = [4, 5]
-    all_words = load_words_from_file("words_all.txt")
+    for i in range(7, 20):
+        chapters = [i, i+1]
+        all_words = load_words_from_file("words_all.txt")
 
-    start_index = 1 + 30 * (chapters[0] - 1)
-    stop_index = 30 * chapters[1] + 1
-    for i in range(start_index, stop_index):
-        print(all_words[i])
+        start_index = 1 + 30 * (chapters[0] - 1)
+        stop_index = 30 * chapters[1] + 1
 
-    words = [all_words[i] for i in range(start_index, stop_index)]
-    shuffled = shuffle_words(words)
-    write_file_from_words(shuffled, f"words{chapters[0]}-{chapters[1]}.txt")
+        words = [all_words[i] for i in range(start_index, stop_index)]
+        shuffled = shuffle_words(words)
+        write_file_from_words(shuffled, f"words{chapters[0]}-{chapters[1]}.txt")
 
 
 if __name__ == "__main__":

0c6eb67802da3b6b322626d41a049962ed0ae9bf
heecheol.park <heecheol.park@linecorp.com>
2022-11-23 14:01:52 +0900

    feat:Use daum-dic

--- words/mp3/make.sh
+++ words/mp3/make.sh
@@ -1,5 +1,3 @@
-#curl -o out.txt https://en.dict.naver.com/#/search?range=all&query=blameki
-curl -o out.txt https://m.search.naver.com/search.naver?query=blame&where=m_ldic&sm=msv_hty
-curl -o out.txt https://dic.daum.net/word/view.do?wordid=ekw000018343&q=blame
+curl -s  https://dic.daum.net/search.do?q=$1 | grep data-audio 
 
 #awk -f awk.script ../words6-7.txt

8f2039619f82b3a36b704a482b2762cc2437cda7
pchuri <pchuri@gmail.com>
2022-11-23 10:14:01 +0900

    wip

---
+++ words/mp3/make.sh
@@ -0,0 +1,5 @@
+#curl -o out.txt https://en.dict.naver.com/#/search?range=all&query=blameki
+curl -o out.txt https://m.search.naver.com/search.naver?query=blame&where=m_ldic&sm=msv_hty
+curl -o out.txt https://dic.daum.net/word/view.do?wordid=ekw000018343&q=blame
+
+#awk -f awk.script ../words6-7.txt

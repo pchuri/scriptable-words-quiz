bf24ea99e52f8224ba708b6ed49b3b363e30b651
heecheol.park <heecheol.park@linecorp.com>
2022-12-06 22:51:08 +0900

    feat(words): Added 20-60

--- python_make_words/main.py
+++ python_make_words/main.py
@@ -6,14 +6,14 @@ from words import (
 
 
 def main():
-    for i in range(7, 20):
+    for i in range(20, 60):
         chapters = [i, i+1]
         all_words = load_words_from_file("words_all.txt")
 
         start_index = 1 + 30 * (chapters[0] - 1)
         stop_index = 30 * chapters[1] + 1
 
-        words = [all_words[i] for i in range(start_index, stop_index)]
+        words = list([all_words[i] for i in range(start_index, stop_index)])
         shuffled = shuffle_words(words)
         write_file_from_words(shuffled, f"words{chapters[0]}-{chapters[1]}.txt")
 
